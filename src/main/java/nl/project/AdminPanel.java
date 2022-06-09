9a3a9929c157eb9f794c1ecd8216c6974e46f293
Profnoobstone <bartdeoud@gmail.com>
2022-06-09 12:45:16 +0200

    AdminPanel add user

--- src/main/java/nl/project/AdminPanel.java
+++ src/main/java/nl/project/AdminPanel.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -13,53 +14,71 @@ import java.io.File;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class SceneController {
-
-
+public class AdminPanel
+{
     private Stage stage;
     private Scene scene;
 
-
-    //Menu
-
-    @FXML
-    Button btnProfile;
-    @FXML
-    Button btnForm;
-    @FXML
-    Button btnRanklist;
-    @FXML
-    Button btnSettings;
-    @FXML
-    Button profielToAdminPanel;
-    @FXML
-    TextField Username;
-    @FXML
-    TextField password;
+    private final DataHandler database = new DataHandler(getDatabase());
 
     @FXML
-    public void profielToAdminPanelClicked(ActionEvent event) throws Exception{
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("AdminPanel.fxml"));
+    Button btnAPGetUsers;
+    @FXML
+    TextArea textAreaUsers;
+    @FXML
+    Button btnChangeUser;
+    @FXML
+    Button btnAddUser;
+    @FXML
+    TextField TFUsername;
+    @FXML
+    TextField TFPassword;
+    @FXML
+    TextField TFPoints;
+    @FXML
+    TextField TFAccesslevel;
+
+    @FXML
+    public void ChangeUser(ActionEvent event) throws Exception {
+
+    }
+
+    @FXML
+    public void AddUser(ActionEvent event) throws Exception {
+        String query = String.format("INSERT INTO [Users] ([UserName], [Password], [Points], [AccessLevel]) VALUES (\"%s\",\"%s\",%s,%s)",TFUsername.getText(),TFPassword.getText(),TFPoints.getText(),TFAccesslevel.getText());
+        database.runQuery(query);
+    }
+
+    public String getDatabase(){
+        String filePath = new File("").getAbsolutePath();
+        return filePath + "/Databases/project2db.mdb";
+    }
+
+    @FXML
+    public void LoadUsers(ActionEvent event) throws Exception {
+        textAreaUsers.setText("");
+        ResultSet rs = database.getData("SELECT * FROM [Users]");
+        try
+        {
+            while (rs.next())
+            {
+                textAreaUsers.setText(textAreaUsers.getText() + String.format("%d - %s\n",rs.getInt("Count"),rs.getString("UserName")));
+            }
+        } catch (SQLException throwables)
+        {
+            throwables.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void ProfileButtonClicked(ActionEvent event) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Profiel.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(fxmlLoader.load());
         stage.setMaximized(true);
         stage.setTitle("Greetings!");
         stage.setScene(scene);
         stage.show();
-    }
-
-    @FXML
-    public void ProfileButtonClicked(ActionEvent event) throws Exception {
-        if(login())
-        {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Profiel.fxml"));
-            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            scene = new Scene(fxmlLoader.load());
-            stage.setMaximized(true);
-            stage.setTitle("Greetings!");
-            stage.setScene(scene);
-            stage.show();
-        }
     }
 
     @FXML
@@ -93,27 +112,5 @@ public class SceneController {
         stage.setTitle("Greetings!");
         stage.setScene(scene);
         stage.show();
-    }
-
-    public boolean login(){
-        String filePath = new File("").getAbsolutePath();
-        String name = Username.getText();
-        String password1 = null;
-        DataHandler database = new DataHandler(filePath + "/Databases/project2db.mdb");
-        ResultSet rs = database.getData("SELECT * FROM [Users]");
-        try
-        {
-            while (rs.next())
-            {
-                if(rs.getString("UserName").equals(name)){
-                    password1 = rs.getString("Password");
-                }
-            }
-        } catch (SQLException throwables)
-        {
-            throwables.printStackTrace();
-        }
-        if(password1 == null) return false;
-        return password.getText().equals(password1);
     }
 }
